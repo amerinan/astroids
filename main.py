39184aba6fdc3ac9450e2aa11a7e8ef1dedeebb7
amerinan <carpediem.ck@gmail.com>
2025-08-20 23:18:37 -0400

    Updateded to Add 60FPS limit

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                  return
              
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill("black")
         pygame.display.flip()
+        
+        #limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000 
+        
         
 if __name__ == "__main__":
     main()
