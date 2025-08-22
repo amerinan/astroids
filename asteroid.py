fcc934f6df20c47edce1bcb358832a8d4bb4474d
amerinan <carpediem.ck@gmail.com>
2025-08-22 19:50:22 -0400

    asteroid update draw method

--- asteroid.py
+++ asteroid.py
@@ -7,7 +7,7 @@ class Asteroid(CircleShape):
         super().__init__(x, y, radius)
         
     def draw(self,screen):
-        pygame.draw.circle(screen, (0,0,0),(self.x,self.y),self.radius,2)
+        pygame.draw.circle(screen, (0,0,0),self.position,self.radius,2)
         
     def update(self, dt):
         self.velocity += (self.velocity * dt)
